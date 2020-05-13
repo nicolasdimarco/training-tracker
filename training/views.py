751d352ba09d319e970f1b53bd34758ad4289f1a
Nicolas Dimarco <nicolasdimarco@fiqus.com>
2020-05-13 13:24:59 -0300

    basic creation of training added

--- training/views.py
+++ training/views.py
@@ -1,7 +1,25 @@
+import datetime
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-
+from django.forms import ModelForm
+from django import forms
+from routine.models import Routine
 from training.models import Training
+
+
+class TrainingForm(ModelForm):
+    routine = forms.ModelChoiceField(label='Rutina', queryset=Routine.objects.all())
+    date = forms.DateField(label='Fecha', initial=datetime.date.today)
+    duration = forms.DurationField(label='Duración', initial="01:00:00")
+
+    def __init__(self, *args, **kwargs):
+        super(TrainingForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
+    class Meta:
+        model = Training
+        fields = ['routine', 'date', 'duration']
 
 
 @login_required
@@ -11,17 +29,20 @@ def dashboard(request):
 
 @login_required
 def training_list(request):
-    trainings = Training.objects.filter(user=request.user)
+    trainings = Training.objects.filter(user=request.user).order_by('-date', 'duration')
     return render(request, 'training_list.html', {'trainings': trainings})
 
 
 @login_required
 def training_create(request):
     if request.method == 'POST':
-        pass
-    else:
-        pass
-    return render(request, 'training_create.html', {'trainings': trainings})
+        form = TrainingForm(request.POST)
+        if form.is_valid():
+            training = form.save(commit=False)
+            training.user = request.user
+            training.save()
+            return redirect("/training")
+    return render(request, 'training_create.html', {'form': TrainingForm()})
 
 
 @login_required
