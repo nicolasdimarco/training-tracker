b85939dd68b5a3f742f269fb5c1b836a7722ddf4
Nicolas Dimarco <nicolasdimarco@fiqus.com>
2020-05-13 09:49:09 -0300

    duration field changed in training model

--- training/models.py
+++ training/models.py
@@ -9,7 +9,7 @@ class Training(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, verbose_name='user')
     routine = models.ForeignKey(Routine, on_delete=models.CASCADE, null=False, blank=False, verbose_name='routine')
     date = models.DateField('date', default=datetime.date.today)
-    duration = models.TimeField('duration')
+    duration = models.DurationField('duration', null=False, blank=False)
 
     class Meta:
         verbose_name = 'training'
