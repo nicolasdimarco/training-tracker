7259b9f98da6b2b974a653a05b393735665ac219
Nicolas Dimarco <nicolasdimarco@fiqus.com>
2020-05-17 23:00:36 -0300

    create and edit routine instructions added youtube video links handler added

--- routine/views.py
+++ routine/views.py
@@ -19,8 +19,24 @@ class RoutineForm(ModelForm):
         fields = ['name', 'description']
 
 
+class RoutineInstructionForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(RoutineInstructionForm, self).__init__(*args, **kwargs)
+        self.fields['title'].label = "Titulo"
+        self.fields['description'].label = "Descripcion"
+        self.fields['video_link'].label = "Link al video"
+        self.fields['video_link'].widget.attrs['placeholder'] = "Ingrese solo videos de Youtube"
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
+    class Meta:
+        model = RoutineInstruction
+        fields = ['title', 'description', 'video_link']
+
+
 @login_required
-def routines_list(request):
+def routine_list(request):
     routines = Routine.objects.all()
     return render(request, 'routine_list.html', {'routines': routines})
 
@@ -69,4 +85,39 @@ def routine_instruction_list(request, pk):
         instructions = RoutineInstruction.objects.filter(routine=routine)
         return render(request, 'routine_instruction_list.html', {'instructions': instructions, 'routine': routine})
     except Routine.DoesNotExist:
+        return redirect("/routine")
+
+
+@login_required
+def routine_instruction_create(request, pk):
+    if not request.user.is_trainer:
+        return redirect("/routine")
+
+    try:
+        routine = Routine.objects.get(pk=pk)
+        if request.method == 'POST':
+            form = RoutineInstructionForm(request.POST)
+            if form.is_valid():
+                instruction = form.save(commit=False)
+                instruction.routine = routine
+                instruction.save()
+                return redirect("/routine/{}/instruction".format(pk))
+        return render(request, 'routine_instruction_create.html',
+                      {'form': RoutineInstructionForm(), 'routine': routine})
+    except Routine.DoesNotExist:
+        return redirect("/routine")
+
+
+@login_required
+def routine_instruction_edit(request, routine_pk, instruction_pk):
+    try:
+        instruction = RoutineInstruction.objects.get(pk=instruction_pk, routine__id=routine_pk)
+        if request.method == 'POST':
+            form = RoutineInstructionForm(request.POST, instance=instruction)
+            if form.is_valid():
+                form.save()
+                return redirect("/routine/{}/instruction".format(routine_pk))
+        return render(request, 'routine_instruction_edit.html', {'form': RoutineInstructionForm(instance=instruction),
+                                                                 'instruction': instruction})
+    except RoutineInstruction.DoesNotExist:
         return redirect("/routine")
